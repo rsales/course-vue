d4a2ca0797cb61dc5c889cb8589cdcbc77d75bab
Rafael Sales <rafael.sales93@hotmail.com>
2019-04-28 17:07:42 -0300

    Course before 'challenge-05'

---
+++ 02_conditionals-and-list/challenge-05/app.js
@@ -0,0 +1,24 @@
+new Vue({
+	el: '#challenge',
+	data: {
+		// challenge 1
+		visible: false,
+
+		// challenge 2
+		names: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+
+		// challenge 3
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		
+		// challenge 4
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		}
+	}
+});
