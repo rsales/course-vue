f53f5781176bfb1ef1201060cc5501cf6223649f
Rafael Sales <rafael.sales93@hotmail.com>
2019-05-15 23:16:53 -0300

    Project1 include animate.css

--- 03_project-01/assets/javascripts/game.js
+++ 03_project-01/assets/javascripts/game.js
@@ -1,6 +1,24 @@
+
 new Vue({
 	el: '#game',
 	data: {
+		players: [
+			{ 
+				name: 'player1',
+				image:'assets/images/player1.svg',
+				select: true,
+			},
+			{
+				name: 'player2',
+				image: 'assets/images/player2.svg',
+				select: false,
+			},
+			{
+				name: 'player3',
+				image: 'assets/images/player3.svg',
+				select: false,
+			}
+		]
 	},
 	methods: {
 
