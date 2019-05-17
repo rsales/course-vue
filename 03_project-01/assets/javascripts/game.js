9665599888788746205f745b4a236bea7151c2e8
Rafael Sales <rafael.sales93@hotmail.com>
2019-05-17 01:23:25 -0300

    Implement firsts actions.

--- 03_project-01/assets/javascripts/game.js
+++ 03_project-01/assets/javascripts/game.js
@@ -2,6 +2,7 @@
 new Vue({
 	el: '#game',
 	data: {
+		muted: false,
 		players: [
 			{ 
 				name: 'player1',
@@ -18,10 +19,26 @@ new Vue({
 				image: 'assets/images/player3.svg',
 				select: false,
 			}
-		]
+		],
+		playerSelect: 0,
 	},
 	methods: {
-
+		music() {
+			this.muted = this.muted == false ? true : false
+		},		
+		selectPlay(val) {
+			document.getElementById('p' + val).checked = true;
+			this.playerSelect = val;
+		}
+	},
+	watch: {
+		playerSelect(news, olds) {
+			this.players[news].select = true
+			this.players[olds].select = false
+		},
+		muted(news, olds) {
+			document.querySelector("#controller > audio").muted = news
+		}
 	},
 	computed: {
 	},
