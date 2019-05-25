425745b5de3302f8ce9db8ac165e76d343d11115
Rafael Sales <rafael.sales93@hotmail.com>
2019-05-25 19:40:39 -0300

    Include all audios and start game.

--- 03_project-01/assets/javascripts/game.js
+++ 03_project-01/assets/javascripts/game.js
@@ -23,6 +23,13 @@ new Vue({
 		],
 		playerSelect: 0,
 	},
+	// Baixar o audio da música de fundo ao montar o layout
+	mounted: function () {
+		document.querySelector("#audio-select").play()
+		document.querySelector("#audio-select").volume = 0.2
+		document.querySelector("#audio-choosing").volume = 0.3
+		document.querySelector("#audio-start").volume = 0.3
+	},
 	methods: {
 		// Função para alterar o icone de ativar e desativar o audio
 		music() {
@@ -32,14 +39,43 @@ new Vue({
 		selectPlay(val) {
 			document.getElementById('p' + val).checked = true;
 			this.playerSelect = val;
+
+			// Efeito sonoro de seleção
+			document.querySelector("#audio-choosing").play()
 		},
 		// Função para iniciar o jogo
 		startGame() {
-			this.start = true
+			this.start = this.start == false ? true : false
 
 			// Alterando 'soundtrack'
 			document.querySelector("#audio-select").pause()
 			document.querySelector("#audio-start").play()
+		},
+		// Função para ataque
+		attack() {
+
+			// Efeito sonoro de ataque
+			document.querySelector("#audio-atack").play()
+		},
+		// Função para ataque especial
+		specialAttack() {
+
+			// Efeito sonoro de ataque especial
+			document.querySelector("#audio-specialAtack").play()
+		},
+		// Função para curar
+		heal() {
+
+			// Efeito sonoro de cura
+			// document.querySelector("#audio-specialAtack").play()
+		},
+		// Função para fugir da batalha
+		endGame() {
+			this.start = this.start == false ? true : false
+
+			// Alterando 'soundtrack'
+			document.querySelector("#audio-select").play()
+			document.querySelector("#audio-start").pause()
 		}
 	},
 	watch: {
