90915ffcba1a6f717cc103e873c0c7bc904102bc
Rafael Sales <rafael.sales93@hotmail.com>
2019-05-25 00:59:36 -0300

    Refined functions.

--- 03_project-01/assets/javascripts/game.js
+++ 03_project-01/assets/javascripts/game.js
@@ -3,6 +3,7 @@ new Vue({
 	el: '#game',
 	data: {
 		muted: false,
+		start: false,
 		players: [
 			{ 
 				name: 'player1',
@@ -23,21 +24,34 @@ new Vue({
 		playerSelect: 0,
 	},
 	methods: {
+		// Função para alterar o icone de ativar e desativar o audio
 		music() {
 			this.muted = this.muted == false ? true : false
-		},		
+		},
+		// Função para marcar o 'radio button' do player selecionado
 		selectPlay(val) {
 			document.getElementById('p' + val).checked = true;
 			this.playerSelect = val;
+		},
+		// Função para iniciar o jogo
+		startGame() {
+			this.start = true
+
+			// Alterando 'soundtrack'
+			document.querySelector("#audio-select").pause()
+			document.querySelector("#audio-start").play()
 		}
 	},
 	watch: {
+		// Função para definir qual personagem foi selecionado
 		playerSelect(news, olds) {
 			this.players[news].select = true
 			this.players[olds].select = false
 		},
-		muted(news, olds) {
-			document.querySelector("#controller > audio").muted = news
+		// Função para selecionar o audio e deixar 'muted'
+		muted(news) {
+			document.querySelector("#controller > #audio-select").muted = news
+			document.querySelector("#controller > #audio-start").muted = news
 		}
 	},
 	computed: {
