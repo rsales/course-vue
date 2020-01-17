951bc98b38cf261f2aaf95dbd8a1ea872edb2857
Rafael Sales <rafael.sales93@hotmail.com>
2020-01-17 01:05:31 -0300

    Inclde action in game.

--- 03_project-01/assets/javascripts/game.js
+++ 03_project-01/assets/javascripts/game.js
@@ -33,6 +33,11 @@ new Vue({
 		document.querySelector("#audio-choosing").volume = 0.3
 		document.querySelector("#audio-start").volume = 0.1
 	},
+	computed: {
+		hasResult() {
+			return this.playerLife == 0 || this.monsterLife == 0
+		}
+	},
 	methods: {
 		// Função para alterar o icone de ativar e desativar o audio
 		music() {
@@ -50,34 +55,60 @@ new Vue({
 		startGame() {
 			this.start = this.start == false ? true : false
 
+			this.playerLife = 100
+			this.monsterLife = 100
+
 			// Alterando 'soundtrack'
 			document.querySelector("#audio-select").pause()
 			document.querySelector("#audio-start").play()
 		},
 		// Função para ataque
-		attack() {
-			this.simpleAttack = true
+		attack(especial) {
+			this.hurt('monsterLife', 5, 10, especial)
+			this.hurt('playerLife', 7, 12, false)
 
-			// Efeito animato de ataque
-			setTimeout(() => {
-				this.simpleAttack = false
-			}, 500)
-			// Efeito sonoro de ataque
-			document.querySelector("#audio-atack").play()
+			// Ataque simples
+			if(!especial) {
+				this.simpleAttack = true
+
+				// Efeito animato de ataque
+				setTimeout(() => {
+					this.simpleAttack = false
+				}, 500)
+				// Efeito sonoro de ataque
+				document.querySelector("#audio-atack").play()
+			}
+			// Ataque especial
+			else {
+				this.masterAttack = true
+
+				// Efeito animato de ataque especial
+				setTimeout(() => {
+					this.masterAttack = false
+				}, 2100)
+				// Efeito sonoro de ataque especial
+				document.querySelector("#audio-specialAtack").play()
+			}
 		},
-		// Função para ataque especial
-		specialAttack() {
-			this.masterAttack = true
-
-			// Efeito animato de ataque especial
-			setTimeout(() => {
-				this.masterAttack = false
-			}, 2100)
-			// Efeito sonoro de ataque especial
-			document.querySelector("#audio-specialAtack").play()
+		// Função para gerar ataque
+		hurt(prop, min, max, especial) {
+			const plus = especial ? 5 : 0
+			const hurt = this.getRandom(min + plus, max + plus)
+			this[prop] = Math.max(this[prop] - hurt, 0)
+		},
+		// Função para gerar valores aleatórios
+		getRandom(min, max) {
+			const value = Math.random() * (max - min) + min
+			return Math.round(value)
+		},
+		healAndHurt() {
+			this.heal(10, 15)
+			this.hurt('playerLife', 7, 12, false)
 		},
 		// Função para curar
-		heal() {
+		heal(min, max) {
+			const heal = this.getRandom(min, max)
+			this.playerLife = Math.min(this.playerLife + heal, 100)
 
 			// Efeito sonoro de cura
 			// document.querySelector("#audio-specialAtack").play()
@@ -92,6 +123,12 @@ new Vue({
 		}
 	},
 	watch: {
+		// Função para escutar hasResult
+		// hasResult(value) {
+		// 	if(value) {
+		// 		this.start = false
+		// 	}
+		// },
 		// Função para definir qual personagem foi selecionado
 		playerSelect(news, olds) {
 			this.players[news].select = true
@@ -102,7 +139,5 @@ new Vue({
 			document.querySelector("#controller > #audio-select").muted = news
 			document.querySelector("#controller > #audio-start").muted = news
 		}
-	},
-	computed: {
-	},
+	}
 })
